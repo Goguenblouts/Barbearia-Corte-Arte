e48192d2295761920484a50cd8812c55a941ac54
Goguenblouts <135179995+Goguenblouts@users.noreply.github.com>
2023-11-18 10:49:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+    // Adiciona um evento de clique ao botão
+    document.getElementById("meuBotao").addEventListener("click", function() {
+        alert("Botão clicado!");
+        // Aqui você pode adicionar a lógica para redirecionar para outra página, abrir uma janela, etc.
+      });
